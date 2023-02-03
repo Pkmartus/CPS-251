576d79da020f3eb3695289ab129de5f38f01a6aa
Pkmartus <pkmartus@gmail.com>
2023-02-03 10:46:18 -0500

    Chapter 33 example Event

---
+++ Examples/EventExample/app/src/main/java/com/ebookfrenzy/eventexample/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ebookfrenzy.eventexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ebookfrenzy.eventexample.databinding.ActivityMainBinding
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+//when the button is pressed the text above is changed
+        binding.myButton.setOnClickListener{
+            binding.statusText.text = "Button Clicked"
+        }
+        //adding a long click method
+        binding.myButton.setOnLongClickListener {
+            binding.statusText.text = "Long Button Click"
+            true
+        }
+
+
+    }
+}
