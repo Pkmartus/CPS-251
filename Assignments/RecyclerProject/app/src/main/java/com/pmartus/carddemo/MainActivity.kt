6168d367a9a296ccf0b0d52d575350671929a964
Pkmartus <pkmartus@gmail.com>
2023-03-24 10:43:59 -0400

    Finished Recycler Project including extra credit retention of initial random state

---
+++ Assignments/RecyclerProject/app/src/main/java/com/pmartus/carddemo/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.pmartus.carddemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.pmartus.carddemo.databinding.ActivityMainBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        layoutManager = LinearLayoutManager(this)
+        binding.contentMain.recyclerView.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        binding.contentMain.recyclerView.adapter = adapter
+    }
+}
---
+++ Assignments/RecyclerProject/app/src/main/java/com/pmartus/carddemo/RecyclerAdapter.kt
@@ -0,0 +1,45 @@
+package com.pmartus.carddemo
+
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+
+class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.card_layout, viewGroup, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemTitle.text = Data.getTitle(position)
+        holder.itemDetail.text = Data.getDetails(position)
+        holder.itemImage.setImageResource(Data.getImages(position))
+    }
+
+    override fun getItemCount(): Int {
+        return Data.titles.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        var itemImage: ImageView
+        var itemTitle: TextView
+        var itemDetail: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.itemImage)
+            itemTitle = itemView.findViewById(R.id.itemTitle)
+            itemDetail = itemView.findViewById(R.id.itemDetail)
+            itemView.setOnClickListener{ v: View ->
+                var position: Int = adapterPosition
+                Snackbar.make(v, "Click detected on item $position",
+                    Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            }
+        }
+    }
+}
